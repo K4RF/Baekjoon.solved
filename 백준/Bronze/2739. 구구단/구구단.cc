7b9ef2a119b86537e8ac3b560f1aa84b22f78a8f
K4RF <rpdirwk1025@naver.com>
2022-07-22 01:27:45 +0900

    Update 구구단.cc

--- 백준/Bronze/2739. 구구단/구구단.cc
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -1,10 +1,14 @@
-#include <stdio.h>
-#pragma warning(disable:4996)
-int main()
-{
-    int N,i;
-    scanf("%d", &N);
-    for(i = 1; i <= 9;i++)
-        printf("%d * %d = %d\n", N, i, N*i);
-    return 0;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int a;
+       Scanner sc = new Scanner(System.in);
+       a = sc.nextInt();
+
+       for(int i = 1; i <= 9; i++){
+        System.out.println(a + " * " + i + " = " +(a*i));
+       }
+       sc.close();
+    }
 }
