02aea823d2d7f9742c1f90a4ffdda92d02bd9a4f
K4RF <rpdirwk1025@naver.com>
2022-07-22 00:54:42 +0900

    [Bronze V] Title: A+B - 7, Time: 256 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int T, A ,B;
+       Scanner sc = new Scanner(System.in);
+       T = sc.nextInt();
+
+       for(int i = 1; i <= T; i++){
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.println("Case #" + i + ": " + (A + B));
+       }
+       sc.close();
+    }
+}
