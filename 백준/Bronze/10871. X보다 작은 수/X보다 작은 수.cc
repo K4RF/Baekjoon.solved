32e397709dbf656fcd041a8fa9e52aa6aa85feba
K4RF <rpdirwk1025@naver.com>
2022-07-22 01:23:11 +0900

    Update X보다 작은 수.cc

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,12 +1,20 @@
-#include<stdio.h>
-#pragma warning(disable:4996)
-int main()
-{
-    int A,X,N;
-    scanf("%d %d", &N, &X);
-    for(int i = 1; i <= N; i++){
-        scanf("%d", &A);
-        if(A < X)
-            printf("%d ", A);
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int T, A, B, snum;
+       Scanner sc = new Scanner(System.in);
+       T = sc.nextInt();
+       A = sc.nextInt();
+
+       for(int i = 0; i < T; i++){
+        B = sc.nextInt();
+        if(B < A){
+            snum = B;
+            System.out.print(snum + " ");
+        }
+       }
+       System.out.println();
+       sc.close();
     }
 }
