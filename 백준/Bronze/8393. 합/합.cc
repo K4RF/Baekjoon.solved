8f847f716d30fc945d05d73e46ec228a1378c760
K4RF <rpdirwk1025@naver.com>
2022-07-22 00:41:00 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#pragma warning(disble:4996)
+
+int main(){
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+        sum = sum + i;
+    printf("%d\n", sum);
+}
