5f7f74637dbcd87c91e973e736a86c7d45604ca8
K4RF <rpdirwk1025@naver.com>
2022-07-23 01:44:15 +0900

    Update 나머지.cc

--- 백준/Bronze/3052. 나머지/나머지.cc
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -1,21 +1,27 @@
-#include <stdio.h>
-#pragma warning(disable:4996)
+import java.util.Scanner;
 
-int main() {
-	int arr[10];
-	int count1=0;
-	for (int i = 0; i < 10; i++)
-	{
-		scanf("%d", &arr[i]);
-	} 
+public class Main {
+    public static void main(String[] args) throws Exception {   
+        int count = 0;
+        int count1 = 0;
+        int [] arr = new int[10];
+       Scanner sc = new Scanner(System.in);
 
-	for (int i = 0; i < 10; i++) {
-		int count = 0;
-		arr[i] = arr[i] % 42;
-		for (int j = 0; j < i; j++) {
-			if (arr[i] == arr[j]) count++;
-		}
-		if (count == 0) count1++;
+       for(int i =0; i < 10; i++){
+        arr[i] = sc.nextInt()%42;
+       }
+       for(int i =0; i < 10; i++){
+        count = 0;
+            for(int j = 0; j<i; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                }
+            }
+            if(count == 0){
+                count1++;
+            }
+        }
+       System.out.println(count1);
+		sc.close();
 	}
-	printf("%d", count1);
-	}
+}
