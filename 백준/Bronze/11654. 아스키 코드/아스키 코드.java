443bd56cf7762036fe3d237aa4211fd453d611e9
K4RF <rpdirwk1025@naver.com>
2022-07-24 04:24:04 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int str = sc.next().charAt(0);
+        System.out.println(str);
+
+        sc.close();
+    }
+}
