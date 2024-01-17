0ef501240501fce5a9c6a724cbc00c6eb06cc5eb
Kaarf_o <rpdirwk1025@naver.com>
2024-01-17 14:34:07 +0900

    [Silver II] Title: DNA 비밀번호, Time: 304 ms, Memory: 24436 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,105 +1,100 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int checkArr[];
-    static int myArr[];
-    static int checkSeceret;
+    static int cArr[];      // 비밀번호 체크 배열
+    static int myArr[];     // 현재 상태 배열
+    static int checkSecret; // 판단 변수
+    public static void main(String[] args)throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());   // 문자열 크기
+        int P = Integer.parseInt(st.nextToken());   // 부분 문자열 크기
+        int result = 0;
 
-    public static void main(String[] args) throws NumberFormatException, IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-
-        int Result = 0;
-
-        char A[] = new char[S];
-        checkArr = new int[4];
+        char A[] = new char[S]; // 문자열 데이터
+        cArr = new int[4];
         myArr = new int[4];
-
-        checkSeceret = 0;
-        A = bf.readLine().toCharArray();
-        st = new StringTokenizer(bf.readLine());
-
-        for(int i =0; i < 4; i++){
-            checkArr[i] = Integer.parseInt(st.nextToken());
-            if(checkArr[i] == 0)
-                checkSeceret++;
+        checkSecret = 0;
+        A = br.readLine().toCharArray();
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 4; i++){
+            cArr[i] = Integer.parseInt(st.nextToken());
+            if(cArr[i] == 0){
+                checkSecret++;
+            }
         }
-        for(int i =0; i< P; i++){
-            Add(A[i]);
+        for(int i =0; i < P; i++){
+            Increase(A[i]);
         }
-        if(checkSeceret == 4){
-            Result++;
-
+        if(checkSecret == 4){
+            result++;
         }
 
         for(int i = P; i < S; i++){
-            int j = i -P;
-            Add(A[i]);
-            Remove(A[j]);
-            if(checkSeceret == 4)
-                Result++;
+            int j = i - P;
+            Increase(A[i]);
+            Decrease(A[j]);
+            if(checkSecret == 4){
+                result++;
+            }
         }
-
-        System.out.println(Result);
-        bf.close();
-
+        System.out.println(result);
+        br.close();
     }
 
-    private static void Add(char c){
+    private static void Increase(char c){
         switch(c){
             case 'A':
                 myArr[0]++;
-                if(myArr[0] == checkArr[0])
-                    checkSeceret++;
+                if(myArr[0] == cArr[0])
+                    checkSecret++;
                 break;
             
             case 'C':
                 myArr[1]++;
-                if(myArr[1] == checkArr[1])
-                    checkSeceret++;
+                if(myArr[1] == cArr[1])
+                    checkSecret++;
                 break;
             
             case 'G':
                 myArr[2]++;
-                if(myArr[2] == checkArr[2])
-                    checkSeceret++;
+                if(myArr[2] == cArr[2])
+                    checkSecret++;
                 break;
             
             case 'T':
                 myArr[3]++;
-                if(myArr[3] == checkArr[3])
-                    checkSeceret++;
+                if(myArr[3] == cArr[3])
+                    checkSecret++;
                 break;
         }
     }
 
-    private static void Remove(char c){
+    private static void Decrease(char c){
         switch(c){
             case 'A':
-                if(myArr[0] == checkArr[0])
-                    checkSeceret--;
+                if(myArr[0] == cArr[0])
+                    checkSecret--;
                 myArr[0]--;
                 break;
             case 'C':
-                if(myArr[1] == checkArr[1])
-                    checkSeceret--;
+                if(myArr[1] == cArr[1])
+                    checkSecret--;
                 myArr[1]--;
                 break;
             case 'G':
-                if(myArr[2] == checkArr[2])
-                    checkSeceret--;
+                if(myArr[2] == cArr[2])
+                    checkSecret--;
                 myArr[2]--;
                 break;
             case 'T':
-                if(myArr[3] == checkArr[3])
-                    checkSeceret--;
+                if(myArr[3] == cArr[3])
+                    checkSecret--;
                 myArr[3]--;
                 break;
         }
